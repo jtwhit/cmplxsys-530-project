c1f72693a81b5e76047cc2bd93f896021d03646d
Jason Whitfield <jtwhit@umich.edu>
2019-04-15 20:46:16 -0400

    Distribution generation optimization.

--- src/distribution.cpp
+++ src/distribution.cpp
@@ -2,20 +2,29 @@
 #include <random>
 #include <cmath>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
 
-pair<int, set<int>> generate_info(int min, int max, int length, double std_dev) {
+pair<int, set<int>> generate_info(int info_min, int info_max, int length, double std_dev) {
     random_device rd;
     default_random_engine random_gen(rd());
 
-    uniform_int_distribution<> uniform(min, max);
+    uniform_int_distribution<> uniform(info_min, info_max);
     int topic = uniform(random_gen);
 
-    long double p_total = 0;
-    vector<long double> p_vals;
-    for (int i = min; i <= max; i++) {
-        long double p = exp(-pow(i-topic, 2) / (2*pow(std_dev,2)));
+    double max_distance = sqrt(-2*pow(std_dev,2)*log(numeric_limits<double>::min()));
+
+    int norm_min = static_cast<int>(floor(topic - max_distance));
+    norm_min = max(norm_min, info_min);
+
+    int norm_max = static_cast<int>(ceil(topic + max_distance));
+    norm_max = min(norm_max, info_max);
+
+    double p_total = 0;
+    vector<double> p_vals;
+    for (int i = norm_min; i <= norm_max; i++) {
+        double p = exp(-pow(i-topic, 2) / (2*pow(std_dev,2)));
         p_vals.push_back(p);
         p_total += p;
     }
@@ -23,13 +32,13 @@ pair<int, set<int>> generate_info(int min, int max, int length, double std_dev) 
     set<int> data;
     uniform_real_distribution<> p_dist(0, 1);
     for (int i = 0; i < length; i++) {
-        long double p = p_dist(random_gen) * p_total;
+        double p = p_dist(random_gen) * p_total;
 
-        long double p_sum = 0;
+        double p_sum = 0;
         for (size_t j = 0; j < p_vals.size(); j++) {
             p_sum += p_vals[j];
             if (p_sum >= p) {
-                data.insert(j - min);
+                data.insert(j + norm_min);
                 p_total -= p_vals[j];
                 p_vals[j] = 0;
                 break;
